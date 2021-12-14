6e21714a2435ff7723f3b88a1c4bff0013bd0889
Mubashira Zaman <mobizmn@live.com>
2021-12-14 10:57:38 +0500

    Adding code outline and documentation for C1M1 Assessment

--- stats.c
+++ stats.c
@@ -2,24 +2,21 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.c
+ * @brief Calculates statistics of the given data
  *
- * <Add Extended Description Here>
  *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mubashira Zaman
+ * @date 13-12-2021
  *
  */
-
-
 
 #include <stdio.h>
 #include "stats.h"
@@ -34,10 +31,43 @@ void main() {
                               200, 122, 150, 90,   92,  87, 177, 244,
                               201,   6,  12,  60,   8,   2,   5,  67,
                                 7,  87, 250, 230,  99,   3, 100,  90};
+}
 
-  /* Other Variable Declarations Go Here */
-  /* Statistics and Printing Functions Go Here */
+
+void print_statistics(unsigned char * arr, int length) 
+{
 
 }
 
-/* Add other Implementation File Code Here */
+void print_array(unsigned char * arr, int length)
+{
+ 
+}
+
+unsigned char find_median(unsigned char * arr, int length)
+{
+
+}
+
+unsigned char find_mean(unsigned char * arr, int length) 
+{
+
+}
+
+unsigned char find_maximum(unsigned char * arr, int length)
+{
+
+}
+
+unsigned char find_minimum(unsigned char * arr, int length)
+{
+
+}
+
+void sort_array(unsigned char * arr, int length)
+{
+
+}
+
+
+ 
--- stats.h
+++ stats.h
@@ -2,39 +2,123 @@
  * Copyright (C) 2017 by Alex Fosdick - University of Colorado
  *
  * Redistribution, modification or use of this software in source or binary
- * forms is permitted as long as the files maintain this copyright. Users are 
+ * forms is permitted as long as the files maintain this copyright. Users are
  * permitted to modify this and use it to learn about the field of embedded
  * software. Alex Fosdick and the University of Colorado are not liable for any
- * misuse of this material. 
+ * misuse of this material.
  *
  *****************************************************************************/
 /**
- * @file <Add File Name> 
- * @brief <Add Brief Description Here >
+ * @file stats.h
+ * @brief Header file with function declarations for the stats.c file
  *
- * <Add Extended Description Here>
- *
- * @author <Add FirsName LastName>
- * @date <Add date >
+ * @author Mubashira Zaman
+ * @date 13-12-2021
  *
  */
 #ifndef __STATS_H__
 #define __STATS_H__
 
-/* Add Your Declarations and Function Comments here */ 
 
 /**
- * @brief <Add Brief Description of Function Here>
+ * @brief Print the statistics of the data in the array
  *
- * <Add Extended Description Here>
+ * Given an array of data and a length, 
+ * print the statistics of the array
+ * such as minimum, maximum, mean, and median values
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
  *
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- * @param <Add InputName> <add description here>
- *
- * @return <Add Return Informaiton here>
+ * @return 
  */
+void print_statistics(unsigned char arr [], int length);
 
+
+/**
+ * @brief Print the array
+ *
+ * Given an array of data and a length, 
+ * print the contents of the array
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return 
+ */
+void print_array(unsigned char arr [], int length);
+
+
+/**
+ * @brief Find median value in the array
+ *
+ * Given an array of data and a length, 
+ * return the median value in the array
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return the median value
+ */
+unsigned char find_median(unsigned char * arr, int length);
+
+
+/**
+ * @brief Find mean value of the array contents
+ *
+ * Given an array of data and a length, 
+ * return the mean value in the array
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return the mean value
+ */
+unsigned char find_mean(unsigned char * arr, int length);
+
+
+/**
+ * @brief Find maximum value in the array
+ *
+ * Given an array of data and a length, 
+ * return the maximum value in the array
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return the maximum value
+ */
+unsigned char find_maximum(unsigned char * arr, int length);
+
+
+/**
+ * @brief Find minimum value in the array
+ *
+ * Given an array of data and a length, 
+ * return the minimum value in the array
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return the minimum value
+ */
+unsigned char find_minimum(unsigned char * arr, int length);
+
+
+/**
+ * @brief Sorts the array in descending order
+ *
+ * Given an array of data and a length, this function 
+ * sorts the array from largest to smallest.
+ * The zeroth Element should be the largest value,
+ * and the last element (n-1) should be the smallest value.
+ * It modifies the input array instead of creating a new one.
+ * 
+ * @param arr       Pointer to input array
+ * @param length    Length of input array
+ *
+ * @return
+ */
+void sort_array(unsigned char * arr, int length);
 
 #endif /* __STATS_H__ */
